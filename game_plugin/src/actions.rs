ef85e8ec5d6e880f304eab303755d6fd678429e9
Jeremiah Senkpiel <fishrock123@rocketmail.com>
2021-04-06 23:59:55 -0700

    refactor: simplify movement actions

--- game_plugin/src/actions.rs
+++ game_plugin/src/actions.rs
@@ -17,54 +17,28 @@ pub struct Actions {
 }
 
 fn set_movement_actions(mut actions: ResMut<Actions>, keyboard_input: Res<Input<KeyCode>>) {
-    if keyboard_input.just_released(KeyCode::W)
-        || keyboard_input.pressed(KeyCode::W)
-        || keyboard_input.just_released(KeyCode::A)
-        || keyboard_input.pressed(KeyCode::A)
-        || keyboard_input.just_released(KeyCode::S)
-        || keyboard_input.pressed(KeyCode::S)
-        || keyboard_input.just_released(KeyCode::D)
-        || keyboard_input.pressed(KeyCode::D)
-    {
-        let mut player_movement = Vec2::ZERO;
+    let mut player_movement = actions.player_movement.unwrap_or(Vec2::ZERO);
 
-        if keyboard_input.just_released(KeyCode::W) || keyboard_input.just_released(KeyCode::S) {
-            if keyboard_input.pressed(KeyCode::W) {
-                player_movement.y = 1.;
-            } else if keyboard_input.pressed(KeyCode::S) {
-                player_movement.y = -1.;
-            } else {
-                player_movement.y = 0.;
-            }
-        } else if keyboard_input.just_pressed(KeyCode::W) {
-            player_movement.y = 1.;
-        } else if keyboard_input.just_pressed(KeyCode::S) {
-            player_movement.y = -1.;
-        } else {
-            player_movement.y = actions.player_movement.unwrap_or(Vec2::ZERO).y;
-        }
+    player_movement.y = 0.;
+    player_movement.x = 0.;
 
-        if keyboard_input.just_released(KeyCode::D) || keyboard_input.just_released(KeyCode::A) {
-            if keyboard_input.pressed(KeyCode::D) {
-                player_movement.x = 1.;
-            } else if keyboard_input.pressed(KeyCode::A) {
-                player_movement.x = -1.;
-            } else {
-                player_movement.x = 0.;
-            }
-        } else if keyboard_input.just_pressed(KeyCode::D) {
-            player_movement.x = 1.;
-        } else if keyboard_input.just_pressed(KeyCode::A) {
-            player_movement.x = -1.;
-        } else {
-            player_movement.x = actions.player_movement.unwrap_or(Vec2::ZERO).x;
-        }
+    if keyboard_input.pressed(KeyCode::W) {
+        player_movement.y += 1.;
+    }
+    if keyboard_input.pressed(KeyCode::S) {
+        player_movement.y += -1.;
+    }
+    if keyboard_input.pressed(KeyCode::D) {
+        player_movement.x += 1.;
+    }
+    if keyboard_input.pressed(KeyCode::A) {
+        player_movement.x += -1.;
+    }
 
-        if player_movement != Vec2::ZERO {
-            player_movement = player_movement.normalize();
-            actions.player_movement = Some(player_movement);
-        }
-    } else {
+    if player_movement == Vec2::ZERO {
         actions.player_movement = None;
+    } else {
+        player_movement = player_movement.normalize();
+        actions.player_movement = Some(player_movement);
     }
 }
